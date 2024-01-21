91f55ecc518acbc11b8ed5397cbededdffa07b4a
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2024-01-21 14:30:24 -0300

    feat(public/robots.txt/components/PokemonCard/index.jsx/pages/Home.jsx/App.test.js/reportWebVitals.js/setupTests.js): Organizando formatacao de texto, e adicionado algumas pastas do react removidas por engano -@angeloneves19

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -10,7 +10,7 @@ export default function PokemonCard({ name, image, types }) {
   return (
     <Card sx={{ maxWidth: 345}}>
       <CardActionArea>
-        <CardMedia component="img" height="200" image={image} alt="green iguana" />
+        <CardMedia component="img" height="100%" image={image} alt="Pokemon" />
         <CardContent>
           <Box display="flex" justifyContent="space-between" alignItems="center">
             <Typography gutterBottom variant="h5" component="div">{name}</Typography>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ export const Home = ({ setPokemonData }) => {
 
   const getPokemons = () => {
     var endpoints = [];
-    for (var i = 1; i < 200; i++) {
+    for (var i = 1; i < 205; i++) {
       endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
     }
     axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((response) => setPokemons(response));
@@ -33,7 +33,6 @@ export const Home = ({ setPokemonData }) => {
         filteredPokemons.push(pokemons[i]);
       }
     }
-
     setPokemons(filteredPokemons);
   };
 
